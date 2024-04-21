ca69ef38197e288a3687f3f7cf62c97671ead83a
mchomans <01168876@pw.edu.pl>
2024-04-21 18:19:25 +0200

    add user effector class

---
+++ src/main/java/com/aquarium/aquarium_backend/databaseTables/UserEffectors.java
@@ -0,0 +1,89 @@
+package com.aquarium.aquarium_backend.databaseTables;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class UserEffectors {
+    private @Id @GeneratedValue int userEffectorTypeId;
+
+    @ManyToOne
+    @JoinColumn(name = "effectorTypeId")
+    private EffectorType effectorType;
+
+    @ManyToOne
+    @JoinColumn(name = "aquariumId")
+    private Aquarium aquarium;
+
+    private float effectorValue;
+    private String effectorControlType;
+
+    public UserEffectors(EffectorType effectorType, Aquarium aquarium, float effectorValue,
+            String effectorControlType) {
+        this.effectorType = effectorType;
+        this.aquarium = aquarium;
+        this.effectorValue = effectorValue;
+        this.effectorControlType = effectorControlType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userEffectorTypeId, effectorType, aquarium, effectorValue, effectorControlType);
+    }
+
+    @Override
+    public boolean equals(Object comparedObject) {
+        if (this == comparedObject)
+            return true;
+        if (comparedObject == null || comparedObject.getClass() != UserEffectors.class)
+            return false;
+        var comparedUserEffectors = (UserEffectors) comparedObject;
+        return userEffectorTypeId == comparedUserEffectors.userEffectorTypeId
+                && effectorType.equals(comparedUserEffectors.effectorType)
+                && aquarium.equals(comparedUserEffectors.aquarium)
+                && effectorValue == comparedUserEffectors.effectorValue
+                && effectorControlType.equals(comparedUserEffectors.effectorControlType);
+    }
+
+    public int getUserEffectorTypeId() {
+        return this.userEffectorTypeId;
+    }
+
+    public EffectorType getEffectorType() {
+        return this.effectorType;
+    }
+
+    public void setEffectorType(EffectorType effectorType) {
+        this.effectorType = effectorType;
+    }
+
+    public Aquarium getAquarium() {
+        return this.aquarium;
+    }
+
+    public void setAquarium(Aquarium aquarium) {
+        this.aquarium = aquarium;
+    }
+
+    public float getEffectorValue() {
+        return this.effectorValue;
+    }
+
+    public void setEffectorValue(float effectorValue) {
+        this.effectorValue = effectorValue;
+    }
+
+    public String getEffectorControlType() {
+        return this.effectorControlType;
+    }
+
+    public void setEffectorControlType(String effectorControlType) {
+        this.effectorControlType = effectorControlType;
+    }
+
+}
