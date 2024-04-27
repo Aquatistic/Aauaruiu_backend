c3b3e30e29f8dd76970d8c17d0fd48c5e2c84443
pkutyl <01168894@pw.edu.pl>
2024-04-27 18:26:10 +0200

    Add test-containers remote support

--- src/test/java/com/aquarium/aquarium_backend/RepositoryTests/UserRepostioryTest.java
+++ src/test/java/com/aquarium/aquarium_backend/RepositoryTests/UserRepostioryTest.java
@@ -13,7 +13,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @Testcontainers
-@ActiveProfiles("test-containers")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepostioryTest {
 
