3216f539eed43775442bc09de9001a5c7bb368a1
pkutyl <01168894@pw.edu.pl>
2024-04-28 20:26:38 +0200

    Add finding aquarium by userId - repository level

--- src/main/java/com/aquarium/aquarium_backend/Repositories/AquariumRepository.java
+++ src/main/java/com/aquarium/aquarium_backend/Repositories/AquariumRepository.java
@@ -1,10 +1,14 @@
 package com.aquarium.aquarium_backend.Repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.aquarium.aquarium_backend.databaseTables.Aquarium;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-@Repository
+import java.util.List;
+
 public interface AquariumRepository extends JpaRepository<Aquarium, Long> {
+    @Query("SELECT a from Aquarium a WHERE a.user.userId = :userId")
+    List<Aquarium> findAquariumsByUserId(Long userId);
+
+
 }
