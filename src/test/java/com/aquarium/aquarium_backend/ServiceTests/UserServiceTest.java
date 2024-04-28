a8a22e01d2259ec9fb7b9d479bc826f83c210cd9
pkutyl <01168894@pw.edu.pl>
2024-04-28 03:10:55 +0200

    Remove unnecessary test containers in unit tests

--- src/test/java/com/aquarium/aquarium_backend/ServiceTests/UserServiceTest.java
+++ src/test/java/com/aquarium/aquarium_backend/ServiceTests/UserServiceTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import com.aquarium.aquarium_backend.databaseTables.User;
 
 
@@ -19,7 +17,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
 
 
-@Testcontainers
 @SpringBootTest
 public class UserServiceTest {
     @Autowired
