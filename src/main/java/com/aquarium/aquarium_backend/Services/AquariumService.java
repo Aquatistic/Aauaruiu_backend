381b703cbe14baad0acf309e4ffc5a1f708d92cf
pkutyl <01168894@pw.edu.pl>
2024-04-28 21:55:10 +0200

    Add getting Aquarium by userId - service level

---
+++ src/main/java/com/aquarium/aquarium_backend/Services/AquariumService.java
@@ -0,0 +1,24 @@
+package com.aquarium.aquarium_backend.Services;
+
+import com.aquarium.aquarium_backend.Repositories.AquariumRepository;
+import com.aquarium.aquarium_backend.databaseTables.Aquarium;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AquariumService {
+
+    private final AquariumRepository aquariumRepository;
+
+    @Autowired
+    public AquariumService(AquariumRepository aquariumRepository) {
+        this.aquariumRepository = aquariumRepository;
+    }
+
+    public List<Aquarium> getAllUsersAquariums(Long userId) {
+        return aquariumRepository.findAquariumsByUserId(userId);
+    }
+
+}
