d528c8267ebdcc2d509b5ce5ff86af8c47d65a14
pkutyl <01168894@pw.edu.pl>
2024-04-28 23:31:40 +0200

    Update controller to use RestController

--- src/main/java/com/aquarium/aquarium_backend/Controllers/AquariumController.java
+++ src/main/java/com/aquarium/aquarium_backend/Controllers/AquariumController.java
@@ -5,14 +5,14 @@ import com.aquarium.aquarium_backend.databaseTables.Aquarium;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(path = "api/v1/aquariums")
 public class AquariumController {
     private final AquariumService aquariumService;
