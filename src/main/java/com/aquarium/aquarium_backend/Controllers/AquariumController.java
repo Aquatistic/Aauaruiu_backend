a891a0509505a01bbb74c7ef5af62d98264f7d9b
pkutyl <01168894@pw.edu.pl>
2024-04-28 22:48:30 +0200

    Add Implementation of AquariumController and tests

--- src/main/java/com/aquarium/aquarium_backend/Controllers/AquariumController.java
+++ src/main/java/com/aquarium/aquarium_backend/Controllers/AquariumController.java
@@ -1,25 +1,30 @@
 package com.aquarium.aquarium_backend.Controllers;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.aquarium.aquarium_backend.Services.AquariumService;
 import com.aquarium.aquarium_backend.databaseTables.Aquarium;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
-@RequestMapping(path = "api/v1/aquarium")
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "api/v1/aquariums")
 public class AquariumController {
     private final AquariumService aquariumService;
 
+    @Autowired
     public AquariumController(AquariumService aquariumService) {
         this.aquariumService = aquariumService;
     }
 
-    @GetMapping
-    public List<Aquarium> getAllAquariums() {
-        return aquariumService.getAllAquariums();
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<List<Aquarium>> getAquariumByUserId(@PathVariable Long userId) {
+        List<Aquarium> aquariumList = aquariumService.getAllUsersAquariums(userId);
+        return new ResponseEntity<>(aquariumList, HttpStatus.OK);
     }
 }
