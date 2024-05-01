9ce46de87c6e5e3f4bc6d7ea60139a29b2e490a8
mchomans <01168876@pw.edu.pl>
2024-05-01 20:08:23 +0200

    add simple user sensors api

---
+++ src/main/java/com/aquarium/aquarium_backend/Services/UserSensorService.java
@@ -0,0 +1,21 @@
+package com.aquarium.aquarium_backend.Services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.aquarium.aquarium_backend.Repositories.UserSensorRepository;
+import com.aquarium.aquarium_backend.databaseTables.UserSensors;
+
+@Service
+public class UserSensorService {
+    private final UserSensorRepository userSensorRepository;
+
+    public UserSensorService(UserSensorRepository userSensorRepository) {
+        this.userSensorRepository = userSensorRepository;
+    }
+
+    public List<UserSensors> getAllUsers() {
+        return userSensorRepository.findAll();
+    }
+}
