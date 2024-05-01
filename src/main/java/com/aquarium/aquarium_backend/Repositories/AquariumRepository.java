4b240451b0a8475424269d153dd633bbe2b168f8
mchomans <01168876@pw.edu.pl>
2024-05-01 14:49:32 +0200

    add aquarium repositiory

--- src/main/java/com/aquarium/aquarium_backend/Repositories/AquariumRepository.java
+++ src/main/java/com/aquarium/aquarium_backend/Repositories/AquariumRepository.java
@@ -1,14 +1,10 @@
 package com.aquarium.aquarium_backend.Repositories;
 
-import com.aquarium.aquarium_backend.databaseTables.Aquarium;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.aquarium.aquarium_backend.databaseTables.Aquarium;
 
+@Repository
 public interface AquariumRepository extends JpaRepository<Aquarium, Long> {
-    @Query("SELECT a from Aquarium a WHERE a.user.userId = :userId")
-    List<Aquarium> findAquariumsByUserId(Long userId);
-
-
 }
