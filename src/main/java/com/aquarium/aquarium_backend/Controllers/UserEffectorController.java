344c0a4d617bbfe98de471ee1eda5a490a913f4e
pkutyl <01168894@pw.edu.pl>
2024-05-10 01:53:51 +0200

    Add controller level for userEffectors

--- src/main/java/com/aquarium/aquarium_backend/Controllers/UserEffectorController.java
+++ src/main/java/com/aquarium/aquarium_backend/Controllers/UserEffectorController.java
@@ -5,6 +5,7 @@ import com.aquarium.aquarium_backend.databaseTables.UserEffectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +24,11 @@ public class UserEffectorController {
         List<UserEffectors> userEffectors = userEffectorService.getAllUsers();
         return new ResponseEntity<>(userEffectors, HttpStatus.OK);
     }
+
+    @GetMapping("aquariums/{aquariumId}")
+    public ResponseEntity<List<UserEffectors>> getEffectorsByAquariumId(@PathVariable Long aquariumId) {
+        List<UserEffectors> userEffectors = userEffectorService.getUserEffectorsByAquariumId(aquariumId);
+        return new ResponseEntity<>(userEffectors, HttpStatus.OK);
+    }
+
 }
