cf2e51f29caa5b80352bd9d6fdfefb3b67c757b9
mchomans <01168876@pw.edu.pl>
2024-05-10 15:40:47 +0200

    add posting multiple measurements

--- src/main/java/com/aquarium/aquarium_backend/Controllers/MeasurementController.java
+++ src/main/java/com/aquarium/aquarium_backend/Controllers/MeasurementController.java
@@ -2,13 +2,19 @@ package com.aquarium.aquarium_backend.Controllers;
 
 import com.aquarium.aquarium_backend.Services.MeasurementService;
 import com.aquarium.aquarium_backend.databaseTables.Measurement;
+
+import org.assertj.core.groups.Tuple;
+import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping(path = "api/v1/measurements")
@@ -24,4 +30,13 @@ public class MeasurementController {
         List<Measurement> allMeasurements = measurementService.getAllMeasurements();
         return new ResponseEntity<>(allMeasurements, HttpStatus.OK);
     }
+
+    @PostMapping("add")
+    public ResponseEntity<Boolean> addMeasurement(@RequestBody List<Pair<Measurement, Long>> measurements) {
+        if (!measurementService.addMeasurements(measurements)) {
+            return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(true, HttpStatus.CREATED);
+    }
+
 }
