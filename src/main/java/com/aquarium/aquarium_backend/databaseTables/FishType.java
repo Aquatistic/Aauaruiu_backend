9c3a7fa9676e94684a623dcd16a52c9132bd20ad
pkutyl <01168894@pw.edu.pl>
2024-05-12 14:06:30 +0200

    Add FishType class implementation

---
+++ src/main/java/com/aquarium/aquarium_backend/databaseTables/FishType.java
@@ -0,0 +1,44 @@
+package com.aquarium.aquarium_backend.databaseTables;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class FishType {
+    public FishType() {}
+    private @Id @GeneratedValue int fishTypeId;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FishType fishType)) return false;
+        return getFishTypeId() == fishType.getFishTypeId() && Objects.equals(getName(), fishType.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFishTypeId(), getName());
+    }
+
+    public int getFishTypeId() {
+        return fishTypeId;
+    }
+
+    public void setFishTypeId(int fishTypeId) {
+        this.fishTypeId = fishTypeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
