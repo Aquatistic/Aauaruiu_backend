fad99a37c5be30bc35d717b4c53663bd002116ca
mchomans <01168876@pw.edu.pl>
2024-05-15 18:34:01 +0200

    Add connecting to Server-Sent Events from aquarium

--- src/main/java/com/aquarium/aquarium_backend/Controllers/AquariumController.java
+++ src/main/java/com/aquarium/aquarium_backend/Controllers/AquariumController.java
@@ -2,11 +2,14 @@ package com.aquarium.aquarium_backend.Controllers;
 
 import com.aquarium.aquarium_backend.Services.AquariumService;
 import com.aquarium.aquarium_backend.databaseTables.Aquarium;
+
 import java.util.List;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequestMapping(path = "api/v1/aquarium")
@@ -44,5 +47,15 @@ public class AquariumController {
     } catch (Exception e) {
       return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
+  }
+
+  @GetMapping(path = "/connect/{aquariumId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  public ResponseEntity<SseEmitter> connectAquarium(@PathVariable Long aquariumId) {
+    SseEmitter emitter = aquariumService.connectAquarium(aquariumId);
+    if (emitter == null) {
+      return new ResponseEntity<>(null,
+          HttpStatus.BAD_REQUEST);
+    }
+    return new ResponseEntity<>(emitter, HttpStatus.OK);
   }
 }
--- src/main/java/com/aquarium/aquarium_backend/Services/AquariumService.java
+++ src/main/java/com/aquarium/aquarium_backend/Services/AquariumService.java
@@ -4,26 +4,32 @@ import com.aquarium.aquarium_backend.Repositories.AquariumRepository;
 import com.aquarium.aquarium_backend.Repositories.UserRepository;
 import com.aquarium.aquarium_backend.databaseTables.Aquarium;
 import com.aquarium.aquarium_backend.databaseTables.User;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Service
 public class AquariumService {
 
   private final AquariumRepository aquariumRepository;
   private final UserRepository userRepository;
+  private final HashMap<Aquarium, SseEmitter> emitters;
 
   @Autowired
   public AquariumService(AquariumRepository aquariumRepository, UserRepository userRepository) {
 
     this.aquariumRepository = aquariumRepository;
     this.userRepository = userRepository;
+    emitters = new HashMap<>();
   }
 
   public List<Aquarium> getAllUsersAquariums(Long userId) throws Exception {
-    User user =
-        userRepository.findById(userId).orElseThrow(() -> new Exception("User does not exist"));
+    userRepository.findById(userId).orElseThrow(() -> new Exception("User does not exist"));
     return aquariumRepository.findAquariumsByUserId(userId);
   }
 
@@ -31,10 +37,19 @@ public class AquariumService {
     return aquariumRepository.findAll();
   }
 
+  public SseEmitter connectAquarium(Long aquariumId) {
+    Aquarium aquarium = aquariumRepository.findById(aquariumId).orElse(null);
+    if (aquarium == null) {
+      return null;
+    }
+    SseEmitter emitter = new SseEmitter();
+    emitters.put(aquarium, emitter);
+    return emitter;
+  }
+
   public Aquarium addAquarium(String aquariumName, float aquariumCapacity, Long userId)
       throws Exception {
-    User user =
-        userRepository.findById(userId).orElseThrow(() -> new Exception("User does not exist"));
+    User user = userRepository.findById(userId).orElseThrow(() -> new Exception("User does not exist"));
     Aquarium createdAquarium = new Aquarium(aquariumName, aquariumCapacity, user);
     return aquariumRepository.save(createdAquarium);
   }
