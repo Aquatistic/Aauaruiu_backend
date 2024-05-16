486d6689e540ac11bfcbe8a3adbcdc265ce9dde8
mchomans <01168876@pw.edu.pl>
2024-05-16 18:46:43 +0200

    add repositories for new tables

---
+++ src/main/java/com/aquarium/aquarium_backend/Repositories/EnvironmentalRequirementsRepository.java
@@ -0,0 +1,10 @@
+package com.aquarium.aquarium_backend.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.aquarium.aquarium_backend.databaseTables.EnvironmentalRequirements;
+
+@Repository
+public interface EnvironmentalRequirementsRepository extends JpaRepository<EnvironmentalRequirements, Integer> {
+}
