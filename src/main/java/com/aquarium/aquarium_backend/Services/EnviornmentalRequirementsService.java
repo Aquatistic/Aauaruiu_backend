64ea2014f7eead7db6db5a0db054a751f1aa43a2
mchomans <01168876@pw.edu.pl>
2024-05-16 18:56:13 +0200

    add simple methods for getting all of the elements in tables

--- src/main/java/com/aquarium/aquarium_backend/Services/EnviornmentalRequirementsService.java
+++ src/main/java/com/aquarium/aquarium_backend/Services/EnviornmentalRequirementsService.java
@@ -1,8 +1,11 @@
 package com.aquarium.aquarium_backend.Services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.aquarium.aquarium_backend.Repositories.EnvironmentalRequirementsRepository;
+import com.aquarium.aquarium_backend.databaseTables.EnvironmentalRequirements;
 
 @Service
 public class EnviornmentalRequirementsService {
@@ -10,5 +13,9 @@ public class EnviornmentalRequirementsService {
 
     public EnviornmentalRequirementsService(EnvironmentalRequirementsRepository environmentalRequirementsRepository) {
         this.environmentalRequirementsRepository = environmentalRequirementsRepository;
+    }
+
+    public List<EnvironmentalRequirements> getAllRequirements() {
+        return environmentalRequirementsRepository.findAll();
     }
 }
