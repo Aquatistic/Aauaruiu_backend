2e759484262f0a469f9d12ddcdb6f5b3df343157
mchomans <01168876@pw.edu.pl>
2024-05-17 10:25:27 +0200

    Add getting fish by aquarium ID and adding fish to aquarium

--- src/main/java/com/aquarium/aquarium_backend/Repositories/FishRepository.java
+++ src/main/java/com/aquarium/aquarium_backend/Repositories/FishRepository.java
@@ -1,10 +1,15 @@
 package com.aquarium.aquarium_backend.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.aquarium.aquarium_backend.databaseTables.Fish;
 
 @Repository
 public interface FishRepository extends JpaRepository<Fish, Integer> {
+    @Query("Select fsh from Fish fsh where fsh.aquarium.aquariumId= :aquariumId")
+    List<Fish> findFishByAquariumId(Long aquariumId);
 }
