f565eb9b102c7e7719f0ad4c6ef85d710ce8a979
pkutyl <01168894@pw.edu.pl>
2024-05-18 20:36:06 +0200

    Add auth method

--- src/main/java/com/aquarium/aquarium_backend/Services/AuthenticationService.java
+++ src/main/java/com/aquarium/aquarium_backend/Services/AuthenticationService.java
@@ -5,24 +5,33 @@ import com.aquarium.aquarium_backend.Security.AuthenticationRequest;
 import com.aquarium.aquarium_backend.Security.AuthenticationResponse;
 import com.aquarium.aquarium_backend.Security.ReqisterRequest;
 import com.aquarium.aquarium_backend.databaseTables.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
-
-    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService) {
+    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
     }
+
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        User user = userRepository.findByUserName(request.getUsername()).orElseThrow(() -> new UsernameNotFoundException("User not found");
+        String jwtToken = jwtService.generateToken(user);
+        return new AuthenticationResponse(jwtToken);
     }
 
     public AuthenticationResponse register(ReqisterRequest request) {
-        User user = new User(request.getUsername(), request.getEmail(), request.getPassword());
+        User user = new User(request.getUsername(), request.getEmail(), passwordEncoder.encode(request.getPassword()));
         String jwtToken = jwtService.generateToken(user);
         return new AuthenticationResponse(jwtToken);
 
