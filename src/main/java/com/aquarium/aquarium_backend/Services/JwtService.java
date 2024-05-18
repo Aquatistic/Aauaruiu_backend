8408a5e98cd63c93dd3c6c3c8824fb87a8c75a65
pkutyl <01168894@pw.edu.pl>
2024-05-18 17:47:12 +0200

    Lint files and add JwtService implementation

--- src/main/java/com/aquarium/aquarium_backend/Services/JwtService.java
+++ src/main/java/com/aquarium/aquarium_backend/Services/JwtService.java
@@ -1,10 +1,34 @@
 package com.aquarium.aquarium_backend.Services;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
-    public String extractUserEmail(String token){
-        return null;
-    }
+
+  @Value("${jwt.secretKey}")
+  private String secretKey;
+
+  public String extractUserEmail(String token) {
+    return null;
+  }
+
+  private Claims extraclAllClaims(String token) {
+    return Jwts.parserBuilder()
+            .setSigningKey(getSignInKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+  }
+
+  private Key getSignInKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+    return Keys.hmacShaKeyFor(keyBytes);
+  }
 }
