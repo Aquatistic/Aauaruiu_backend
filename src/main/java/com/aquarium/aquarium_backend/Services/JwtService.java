c577d568b5453a19f50ed8d98eb21720fde7ada7
pkutyl <01168894@pw.edu.pl>
2024-05-18 18:11:36 +0200

    Add basic token generation

--- src/main/java/com/aquarium/aquarium_backend/Services/JwtService.java
+++ src/main/java/com/aquarium/aquarium_backend/Services/JwtService.java
@@ -2,24 +2,47 @@ package com.aquarium.aquarium_backend.Services;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
   @Value("${jwt.secretKey}")
   private String secretKey;
+  @Value("${jwt.expirationTime}")
+  private Long expirationTime;
 
   public String extractUserEmail(String token) {
-    return null;
+    return extractClaims(token, Claims::getSubject);
   }
 
-  private Claims extraclAllClaims(String token) {
+  public String generateToken(UserDetails userDetails) {
+    return generateToken(new HashMap<>(), userDetails);
+  }
+
+  public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+    return Jwts
+            .builder()
+            .setClaims(extraClaims)
+            .setSubject(userDetails.getUsername())
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+            .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+            .compact();
+  }
+
+  private Claims extractAllClaims(String token) {
     return Jwts.parserBuilder()
             .setSigningKey(getSignInKey())
             .build()
@@ -27,6 +50,10 @@ public class JwtService {
             .getBody();
   }
 
+  public <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = extractAllClaims(token);
+    return claimsResolver.apply(claims);
+  }
   private Key getSignInKey() {
     byte[] keyBytes = Decoders.BASE64.decode(secretKey);
     return Keys.hmacShaKeyFor(keyBytes);
