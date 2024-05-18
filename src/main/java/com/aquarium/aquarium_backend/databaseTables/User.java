625f811d4d80991a4d030778eb47250672571dbf
pkutyl <01168894@pw.edu.pl>
2024-05-18 16:15:08 +0200

    Add impl of UserDetails in User class

--- src/main/java/com/aquarium/aquarium_backend/databaseTables/User.java
+++ src/main/java/com/aquarium/aquarium_backend/databaseTables/User.java
@@ -5,11 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 import java.util.Objects;
 
 @Entity
 @Table(name = "AppUser")
-public class User {
+public class User implements UserDetails {
   public User() {}
 
   private @Id @GeneratedValue Long userId;
@@ -49,18 +53,10 @@ public class User {
     return this.userId;
   }
 
-  public String getUserName() {
-    return this.userName;
-  }
-
   public String getUserEmail() {
     return this.email;
   }
 
-  @JsonIgnore
-  public String getUserPassword() {
-    return this.password;
-  }
 
   public void setUserName(String userName) {
     this.userName = userName;
@@ -72,5 +68,41 @@ public class User {
 
   public void setUserPassword(String password) {
     this.password = password;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  @Override
+  @JsonIgnore
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return null;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return false;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return false;
   }
 }
