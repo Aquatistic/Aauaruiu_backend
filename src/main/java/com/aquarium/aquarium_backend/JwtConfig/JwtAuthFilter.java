a665ba75bcc493a071bbb03690a030768b61fa10
pkutyl <01168894@pw.edu.pl>
2024-05-18 17:13:51 +0200

    Add basic jwtFilter implementation

---
+++ src/main/java/com/aquarium/aquarium_backend/JwtConfig/JwtAuthFilter.java
@@ -0,0 +1,37 @@
+package com.aquarium.aquarium_backend.JwtConfig;
+
+import com.aquarium.aquarium_backend.Services.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    JwtAuthFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwtToken;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwtToken = authHeader.substring(7);
+        String userEmail = jwtService.extractUserEmail(jwtToken);
+
+
+
+    }
+}
---
+++ src/main/java/com/aquarium/aquarium_backend/Services/JwtService.java
@@ -0,0 +1,10 @@
+package com.aquarium.aquarium_backend.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUserEmail(String token){
+        return null;
+    }
+}
