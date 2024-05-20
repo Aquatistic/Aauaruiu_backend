f1d0830f687614a5cdc333ef58218bc34da27c64
pkutyl <01168894@pw.edu.pl>
2024-05-20 23:59:00 +0200

    Use lombok instead of setters and getters

--- src/main/java/com/aquarium/aquarium_backend/Security/AuthenticationRequest.java
+++ src/main/java/com/aquarium/aquarium_backend/Security/AuthenticationRequest.java
@@ -1,29 +1,13 @@
 package com.aquarium.aquarium_backend.Security;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthenticationRequest {
-  private String username;
-  private String password;
-
-  public AuthenticationRequest(String username, String password) {
-    this.username = username;
-    this.password = password;
-  }
-
-  public AuthenticationRequest() {}
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    private String username;
+    private String password;
 }
