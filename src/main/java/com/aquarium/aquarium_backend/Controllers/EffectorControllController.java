f1faa86c498a3091acf969516a5c39a0cddccbb5
mchomans <01168876@pw.edu.pl>
2024-05-23 16:31:17 +0200

    add getting all effector controll

--- src/main/java/com/aquarium/aquarium_backend/Controllers/EffectorControllController.java
+++ src/main/java/com/aquarium/aquarium_backend/Controllers/EffectorControllController.java
@@ -1,10 +1,25 @@
 package com.aquarium.aquarium_backend.Controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.aquarium.aquarium_backend.Services.EffectorControllService;
+import com.aquarium.aquarium_backend.databaseTables.EffectorControll;
 
 @RestController
 @RequestMapping(path = "api/v1/effectorControll")
 public class EffectorControllController {
+    private final EffectorControllService effectorControllService;
 
+    public EffectorControllController(EffectorControllService effectorControllService) {
+        this.effectorControllService = effectorControllService;
+    }
+
+    @GetMapping
+    public List<EffectorControll> getEffectorControll() {
+        return effectorControllService.getAllEffectorControll();
+    }
 }
--- src/main/java/com/aquarium/aquarium_backend/Services/EffectorControllService.java
+++ src/main/java/com/aquarium/aquarium_backend/Services/EffectorControllService.java
@@ -1,8 +1,23 @@
 package com.aquarium.aquarium_backend.Services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.aquarium.aquarium_backend.Repositories.EffectorControllRepository;
+import com.aquarium.aquarium_backend.databaseTables.EffectorControll;
 
 @Service
 public class EffectorControllService {
+
+    private final EffectorControllRepository effectorControllRepository;
+
+    public EffectorControllService(EffectorControllRepository effectorControllRepository) {
+        this.effectorControllRepository = effectorControllRepository;
+    }
+
+    public List<EffectorControll> getAllEffectorControll() {
+        return effectorControllRepository.findAll();
+    }
 
 }
