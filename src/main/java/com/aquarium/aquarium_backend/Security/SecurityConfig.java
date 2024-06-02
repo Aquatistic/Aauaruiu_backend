024eafd288a496845d7b4ecae4f4ed3bd6b0195b
pkutyl <01168894@pw.edu.pl>
2024-06-02 16:01:25 +0200

    Unlock websocket endpoints

--- src/main/java/com/aquarium/aquarium_backend/Security/SecurityConfig.java
+++ src/main/java/com/aquarium/aquarium_backend/Security/SecurityConfig.java
@@ -3,12 +3,18 @@ package com.aquarium.aquarium_backend.Security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -26,12 +32,15 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     httpSecurity
         .csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(
+            .cors(Customizer.withDefaults())
+            .authorizeHttpRequests(
             auth ->
                 auth.requestMatchers("/api/v1/auth/**")
                     .permitAll()
                     .requestMatchers("/api/v1/measurements/add")
                     .permitAll()
+                        .requestMatchers("/ws/**")
+                        .permitAll()
                     .requestMatchers("/api/v1/userEffector/connect/**")
                     .permitAll()
                     .anyRequest()
@@ -43,3 +52,4 @@ public class SecurityConfig {
     return httpSecurity.build();
   }
 }
+
